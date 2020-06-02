cfe1cf102ad78fb8d391e6d62df734624e59af49
Mikhail Kirillov <kirillov.mkhl@gmail.com>
2020-06-03 01:05:35 +0300

    thirdteen

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ app.use("/api/practic", require("./routes/api/practic"));
 app.use("/api/profile", require("./routes/api/profile"));
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
 }
 
 app.listen(3000);
